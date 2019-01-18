b5759d938d6339eefbfc6ac8700fd9fc69a7d462
Tom Gautier <tom.gautier@polymtl.ca>
2019-01-18 14:32:37 -0500

    Added basic methods to handle sockets + EventEmitter in Types

--- server/app/services/socket.service.ts
+++ server/app/services/socket.service.ts
@@ -1,13 +1,26 @@
-import { injectable } from "inversify";
+import { injectable, inject } from "inversify";
 import { Socket } from "dgram";
+import { EventEmitter } from "events";
+import Types from "../types";
 
 @injectable()
 export class SocketService {
     private server: SocketIO.Server;
     private sockets: Map<string, SocketIO.Socket> = new Map();
 
+    public constructor(
+        @inject(Types.EventEmitter) private eventEmitter: EventEmitter
+    ) { }
+
     public init(server: SocketIO.Server): void {
         this.server = server;
     }
 
+    public emit(id: string, event: string, ...args: any[]): void {
+        this.server.to(id).emit(event, args);
+    }
+
+    private handleEvent(event: string, socketId: string, ...args: any[]): void {
+        this.eventEmitter.emit(event, socketId, args);
+    }
 }
