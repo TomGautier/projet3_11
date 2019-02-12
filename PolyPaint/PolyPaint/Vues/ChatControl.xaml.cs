6eabaf618c253f3515360de97c4ed7b4d30bf817
Nicolas Coutu <nicolas.coutu@polymtl.ca>
2019-02-12 10:39:59 -0500

    debut integration du chat

---
+++ PolyPaint/PolyPaint/Vues/ChatControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PolyPaint.Vues
+{
+    /// <summary>
+    /// Interaction logic for ChatControl.xaml
+    /// </summary>
+    public partial class ChatControl : UserControl
+    {
+        public ChatControl()
+        {
+            InitializeComponent();
+            var textBox = (TextBox)FindName("chatBox");
+            textBox.TextChanged += (sender, e) =>
+            {
+                textBox.ScrollToEnd();
+            };
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var textBox = (TextBox)FindName("messageText");
+            textBox.Focus();
+        }
+    }
+}
