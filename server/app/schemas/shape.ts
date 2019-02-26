45518f5760c085f675764ac10fb5df6604c6389f
Tom Gautier <tom.gautier@polymtl.ca>
2019-02-26 14:56:46 -0500

    Updated shape schema with its properties

--- server/app/schemas/shape.ts
+++ server/app/schemas/shape.ts
@@ -3,19 +3,29 @@ import { IShapeModel } from '../models/Shape';
 import mongoose = require('mongoose');
 
 export let shapeSchema: Schema = new Schema({
-    conversationId: {
+    id: {
         type: Schema.Types.ObjectId,
         required: true
-    },
-    body: {
-        type: String,
+    }, 
+    drawingSessionId: {
+        type: Schema.Types.ObjectId,
         required: true
     },
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
-    }
-});
+    },
+    properties: {
+        type: { type: String, required: true },
+        fillingColor: { type: String, required: true },
+        borderColor: { type: String, required: true },
+        middlePointCoord: { type: [Number], required: true },
+        height: { type: Number, required: true },
+        width: { type: Number, required: true },
+        rotation: { type: Number }
+    },
+
+}, {timestamps: true});
 
 export default mongoose.connection.model<IShapeModel>('Shape', shapeSchema);
 
