65ca35907443f4a07742b127d1307228d577cc52
Tom Gautier <tom.gautier@polymtl.ca>
2019-03-05 10:11:51 -0500

    Schemas for images

---
+++ server/app/schemas/image.ts
@@ -0,0 +1,30 @@
+import { Schema } from 'mongoose';
+import { IImageModel } from '../models/Image';
+import mongoose = require('mongoose');
+
+export let imageSchema: Schema = new Schema({
+    id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    }, 
+    author: {
+        type: String,
+        ref: 'User',
+    },
+    visibility: {
+        type: String,
+        required: true
+    },
+    protection: {
+        type: String,
+        required: true
+    },
+    shapes: {
+        type: Schema.Types.ObjectId,
+        ref: 'Shape',
+        required: true
+    }
+}, {timestamps: true});
+
+export default mongoose.connection.model<IImageModel>('Image', imageSchema);
+
