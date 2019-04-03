a03f2b43e280d6c2ffd9b05cab45e75fdb65dbf3
Nicolas Coutu <nicolas.coutu@polymtl.ca>
2019-04-03 13:31:13 -0400

    code pour connection serveur des galleries

--- PolyPaint/PolyPaint/Utilitaires/NetworkManager.cs
+++ PolyPaint/PolyPaint/Utilitaires/NetworkManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace PolyPaint.Utilitaires
 {
@@ -25,6 +26,19 @@ namespace PolyPaint.Utilitaires
             var response = await client.PostAsync("http://127.0.0.1:3000/connection/signup/" + username + "/" + password, null);
 
             return await response.Content.ReadAsStringAsync();
+        }
+
+        internal void PostImage(string username, string sessionId, string imageId, string visibility, string protection)
+        {
+            var bodyTemplate = new
+            {
+                id = imageId,
+                visibility = visibility,
+                protection = protection,
+                author = username
+            };
+            var body = new StringContent(JsonConvert.SerializeObject(bodyTemplate), Encoding.UTF8, "application/json");
+            var response =  client.PostAsync("http://127.0.0.1:3000/api/images/" + sessionId + "/" + username, body);
         }
     }
 }
