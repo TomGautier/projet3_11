e814181560e4e5c0338dca9fb11c38848ecb1f5c
Tom Gautier <tom.gautier@polymtl.ca>
2019-04-08 18:13:21 -0400

    method to update protection

--- server/app/services/image.service.ts
+++ server/app/services/image.service.ts
@@ -91,4 +91,18 @@ export class ImageService implements ImageServiceInterface {
 
         return image;
     }
+
+    public async updateProtection(imageId: string, protection: string) {
+        const doc = {
+            id: imageId,
+            protection: protection
+        };
+        try {
+            await this.databaseService.updateProtection(Image, doc);
+        }
+        catch(err) {
+            throw err;
+        }
+        
+    }
 }
